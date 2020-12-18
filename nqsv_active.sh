c17cf41b2d0216642629fdd2fd8f47f23d63b565
Marcelo Garcia <marcelomgarcia@gmail.com>
2020-12-18 11:05:38 +0100

    Check if NQSV services are running
    AWX/Ansible stop the service after updating config file.

---
+++ nqsv_active.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Check if NQSV services are active
+# Marcelo Garcia (NEC)
+
+cmk_status=0
+cmk_check="nqsv_active"
+cmk_msg="${cmk_status} ${cmk_check} - OK: All NQSV services are running."
+
+nqsv_service_list=( nqs-jsv.service nqs-lchd.service nqs-uag.service )
+for ii in "${nqsv_service_list[@]}"
+do
+    is_active=`systemctl is-active $ii`
+    if [[ "$is_active" != "active" ]]; then
+        cmk_status=2
+        cmk_msg="${cmk_status} ${cmk_check} - Critical: service ${ii} not active"
+        break
+    fi
+done
+
+echo ${cmk_msg}
+
+exit 0
